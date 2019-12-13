a091fc844e86f1a554d6704f4edff32c2df87ab0
Athanasios Kandylas <kand-2@hotmail.com>
2019-12-13 13:18:30 +0100

    Change name of the project

--- src/Main.java
+++ src/Main.java
@@ -108,7 +108,7 @@ public class Main {
         return stationsToVisit;
     }
 
-    private static double calculateDistance(double x1, double y1, double x2, double y2) { //Check the Earth's radius for outdoors (For the Discussion)
+    private static double calculateDistance(double x1, double y1, double x2, double y2) {
         double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return distance;
     }
